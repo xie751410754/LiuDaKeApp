c5b87e472cb74c7a8c10238b519e208f02dccf70
xzl <751410754@qq.com>
2021-02-25 16:42:39 +0800

    运营中心界面更新

--- app/src/main/java/com/cdxz/liudake/adapter/store_manager/MerchantAdapter.java
+++ app/src/main/java/com/cdxz/liudake/adapter/store_manager/MerchantAdapter.java
@@ -20,7 +20,7 @@ public class MerchantAdapter extends BaseQuickAdapter<StoreBean, BaseViewHolder>
 
 
         helper.setText(R.id.tv_name,item.getName())
-                .setText(R.id.tv_address,"地址："+item.getAddress())
+                .setText(R.id.tv_address,"区域："+item.getAddress())
                 .setText(R.id.tv_time,"营业时间："+item.getOpen_end_time())
                 .setText(R.id.tv_phone,"联系电话："+item.getContact())
                 .setGone(R.id.divider,helper.getAdapterPosition()!=getData().size()-1);
