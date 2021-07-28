0d74bea23eb0020a82613b73e20aa282e12c5e6b
xzl <751410754@qq.com>
2021-07-28 18:03:21 +0800

    提交代码

--- app/src/main/java/com/cdxz/liudake/ui/my/service/WithdrawalActivity.java
+++ app/src/main/java/com/cdxz/liudake/ui/my/service/WithdrawalActivity.java
@@ -77,6 +77,7 @@ public class WithdrawalActivity extends BaseActivity {
         context.startActivity(intent);
     }
 
+
     @Override
     protected int getContentViewId() {
         return R.layout.activity_withdrawal_new;
